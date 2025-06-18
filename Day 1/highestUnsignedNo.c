428818c18964ab6b044a323c573b45322618c18e
AbinandhanP13 <155878300+AbinandhanP13@users.noreply.github.com>
2025-06-18 22:41:30 +0530

    Update highestUnsignedNo.c

--- Day 1/highestUnsignedNo.c
+++ Day 1/highestUnsignedNo.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main() {
-  unsigned long long int max = (unsigned long long int) (pow(2,64) -1);
+  unsigned long long int max = (unsigned long long int) (pow(2,127) -1);
   printf("Highest number represented by unsigned long long int is %llu\n", max);
   return 0;
 }
